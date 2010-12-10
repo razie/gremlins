d0c6ce4304845358ca2453704f5477a8b02a1d0a
Razvan Cojocaru <razie@razie.com>
2010-12-10 15:49:44 -0500

    scala 2.8.1

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -4,12 +4,13 @@ class PGremlins(info: ProjectInfo) extends DefaultProject(info) {
 
   override def managedStyle = ManagedStyle.Maven
   val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
+  //val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
   Credentials(Path.userHome / ".ivy2.credentials", log)
       
   val scalatest = "org.scalatest" % "scalatest" % "1.2"
   val junit =     "junit"         % "junit"     % "4.5" % "test->default"
   def json =      "org.json"      % "json"      % "20090211"
 
-  val scrip   = "com.razie" %% "scripster"       % "0.4-SNAPSHOT"
+  val scrip   = "com.razie" %% "scripster"       % "0.6-SNAPSHOT"
 }
 
