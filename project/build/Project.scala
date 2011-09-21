9e26fab3540d1c07c7809d97cc674700b53bdc58
Razie <razie@razie.com>
2011-09-20 20:54:30 -0400

    restructure in sub-projects

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,6 +1,7 @@
 import sbt._
 
 class PGremlins(info: ProjectInfo) extends DefaultProject(info) with posterous.Publish {
+
   override def managedStyle = ManagedStyle.Maven
   val publishTo =
     if (version.toString endsWith "-SNAPSHOT")
@@ -8,13 +9,23 @@ class PGremlins(info: ProjectInfo) extends DefaultProject(info) with posterous.P
     else
       "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
   Credentials(Path.userHome / ".ivy2.credentials", log)
-     
+
   val snap = (if (version.toString endsWith "-SNAPSHOT") "-SNAPSHOT" else "")
- 
-  val scalatest = "org.scalatest" % "scalatest_2.9.1" % "1.6.1"
-  val junit =     "junit"         % "junit"     % "4.5" % "test->default"
+
+  val SCALAVER = "2.9.1"
+
+  def scalatest = "org.scalatest" % "scalatest_2.9.1" % "1.6.1"
+  def junit =     "junit"         % "junit"     % "4.5" % "test->default"
   def json =      "org.json"      % "json"      % "20090211"
 
-  val scripster   = "com.razie" %% "scripster"       % ("0.8" + snap)
+  def scripster   = "com.razie" %% "scripster"       % ("0.8" + snap)
+
+  lazy val core = project("core", "gremlins-core", new CoreProject(_))
+  //lazy val gakka  = project("akka",  "gremlins-akka",  new GakkaProject(_), core)
+
+  class CoreProject(info: ProjectInfo) extends DefaultProject(info) {
+    val depys@(sc, ju, js, scr) = (scalatest, junit, json, scripster)
+  }
+
 }
 
