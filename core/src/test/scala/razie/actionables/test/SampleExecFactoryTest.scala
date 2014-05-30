68d478a9547de9ffd0737cd1f0cf51e256149eaa
Razie <razie@razie.com>
2014-05-29 23:31:41 -0400

    updated to scala 2.10.3

---
+++ core/src/test/scala/razie/actionables/test/SampleExecFactoryTest.scala
@@ -0,0 +1,71 @@
+/**
+ * ____    __    ____  ____  ____,,___     ____  __  __  ____
+ *  (  _ \  /__\  (_   )(_  _)( ___)/ __)   (  _ \(  )(  )(  _ \           Read
+ *   )   / /(__)\  / /_  _)(_  )__) \__ \    )___/ )(__)(  ) _ <     README.txt
+ *  (_)\_)(__)(__)(____)(____)(____)(___/   (__)  (______)(____/    LICENSE.txt
+ */
+package razie.actionables.test
+
+import org.scalatest.junit._
+import razie.actionables._
+import razie.actionables.library._
+import razie.actionables._
+import razie.actionables.ActionableSpec
+import razie.base.ActionContext
+
+/**
+ * sample activity - just add X to the argument
+ */
+class ExecX extends razie.gremlins.JWFunc {
+
+  def spec: ActionableSpec = new ActionableSpec(razie.AA("someArg=default"), razie.AA("result"));
+
+  override def apply(in: ActionContext, v: Any): Any = {
+    val msg = in sa "someArg"
+
+    razie Log ("adding X to " + msg)
+
+    msg + "X"
+  }
+}
+
+/**
+ * factory for the basic executables offered
+ */
+object SampleExecutables extends ExecutableFactory {
+
+  /**
+   * make an executable
+   *
+   * @param unifiedstring
+   * @return
+   */
+  override def make(unifiedstring: String): razie.gremlins.JWFunc = {
+    val Executables.pat(domain, cmd, parens, args) = unifiedstring
+
+    require(cmds contains cmd, "need to support command: " + cmd)
+
+    cmd match {
+      case "x" => new ExecX
+    }
+  }
+
+  /**
+   * @param domain
+   * @return the actions in this domain
+   */
+  override def commands(): Array[razie.AI] = cmds map (new razie.AI(_))
+
+  val cmds = Array("x")
+}
+
+class SampleExecFactoryTest extends JUnit3Suite {
+  import Actionables._
+
+  Executables.reg("sample", SampleExecutables)
+
+  def testX: Unit = expect("ExecX") {
+    val o = Executables.make("sample:x someArh=hihi")
+    o.getClass().getSimpleName
+  }
+}
